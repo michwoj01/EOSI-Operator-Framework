362bbb3ba7bc0af2c6946119b6c98403d74af3ec
Michał Wójcik <michwoj01@gmail.com>
2024-06-01 09:09:55 +0200

    fix webapp

--- ecsb-frontend/vite.config.ts
+++ ecsb-frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  }
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: '0.0.0.0',
+        strictPort: true,
+        port: 5173,
+    }
 })
