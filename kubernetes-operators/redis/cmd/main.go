5e0c9bc1790b5b85edcc29f76e7613acde0f380a
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-06-11 02:10:15 +0200

    Everything works

--- kubernetes-operators/redis/cmd/main.go
+++ kubernetes-operators/redis/cmd/main.go
@@ -124,6 +124,7 @@ func main() {
 
 	if err = (&controller.RedisReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Redis"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Redis")
